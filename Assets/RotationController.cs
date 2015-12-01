afea3efef154676c66de66044bce2b4515b0cf70
FeniXb3 <fenix.b3@gmail.com>
2015-12-01 21:32:02 +0100

    Init commit

---
+++ Assets/RotationController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotation = Input.GetAxis("Horizontal");
+        
+        var body = GetComponent<Rigidbody2D>();
+        body.MoveRotation(body.rotation + rotation);
+	}
+}
