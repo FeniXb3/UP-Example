404ca9056d41bac5ca5e3a849167f3f43e83f7cd
FeniXb3 <fenix.b3@gmail.com>
2015-12-06 17:51:41 +0100

    Chaged graphics, added spring joint to camera to follow player - it's not the best idea, as it adds physics calculation for camera move, but a fun thing to check

---
+++ Assets/Scripts/RotationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class RotationController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void FixedUpdate()
+    {
+        float rotation = Input.GetAxis("Horizontal") * speed;
+
+        var body = GetComponent<Rigidbody2D>();
+        body.MoveRotation(body.rotation + rotation);
+
+        //Vector2 movingForce = new Vector2(rotation, 0f);
+        //body.AddForce(movingForce * speed);
+    }
+}
