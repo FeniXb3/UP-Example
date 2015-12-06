1c0e95e08aa3b98cc0d7763d8780531dd3f08ef1
FeniXb3 <fenix.b3@gmail.com>
2015-12-07 00:56:57 +0100

    Added animated coin that gives you 10 points!

---
+++ Assets/Scripts/CollectiblePointsController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CollectiblePointsController : MonoBehaviour
+{
+    public int points = 10;
+
+    private PlayerPointsController playerPointsController;
+
+    void Start()
+    {
+        playerPointsController = GameObject.FindGameObjectWithTag("GameController").GetComponent<PlayerPointsController>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            playerPointsController.AddPoints(points);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPointsController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPointsController : MonoBehaviour
+{
+    public Text pointsLabel;
+    private int points;
+    
+    public void AddPoints(int pointsToAdd)
+    {
+        points += pointsToAdd;
+
+        pointsLabel.text = string.Format("Points: {0}", points);
+    }
+}
