32523c7b40461eb61fd17eaf4e9e1ed9abe2dbe7
FeniXb3 <fenix.b3@gmail.com>
2015-12-07 00:24:37 +0100

    Changed camera to simple following one. REALLY changed gravity scale and speed of Player

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Vector3 offset;
+    public GameObject objectToFollow;
+
+    void Start()
+    {
+        if(objectToFollow == null)
+        {
+            objectToFollow = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    void Update()
+    {
+        transform.position = objectToFollow.transform.position + offset;
+    }
+}
