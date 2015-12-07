5c3ac6ae97f1d2095e8f7e662c46d6aa85eda299
FeniXb3 <fenix.b3@gmail.com>
2015-12-07 01:26:49 +0100

    Coins sound!

--- Assets/Scripts/CollectiblePointsController.cs
+++ Assets/Scripts/CollectiblePointsController.cs
@@ -4,19 +4,34 @@ public class CollectiblePointsController : MonoBehaviour
 {
     public int points = 10;
 
+    private AudioSource audioSource;
     private PlayerPointsController playerPointsController;
+    private SpriteRenderer spriteRenderer;
+    private bool isCollected;
 
     void Start()
     {
         playerPointsController = GameObject.FindGameObjectWithTag("GameController").GetComponent<PlayerPointsController>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if(isCollected && !audioSource.isPlaying)
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if(collision.CompareTag("Player") && !isCollected)
         {
+            audioSource.Play();
             playerPointsController.AddPoints(points);
-            gameObject.SetActive(false);
+            spriteRenderer.enabled = false;
+            isCollected = true;
         }
     }
 }
