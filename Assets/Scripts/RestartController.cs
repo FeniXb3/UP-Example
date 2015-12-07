20cbb819700cd0b4bc668349c4ab0d46278407d2
FeniXb3 <fenix.b3@gmail.com>
2015-12-07 02:16:09 +0100

    Restart level when the player fall

---
+++ Assets/Scripts/RestartController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RestartController : MonoBehaviour
+{
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
